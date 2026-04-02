b4b04bab93daf2093bdde6a46c8eab61d6cdc116
MohamedAzizMaamouri <mohamedaziz.maamouri@insat.ucar.tn>
2026-04-02 14:15:53 +0100

    Ajout Role pour user

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -3,18 +3,21 @@ import { Cv } from '../../cv/entities/cv.entity';
 
 @Entity('users')
 export class User {
-    @PrimaryGeneratedColumn()
-    id: number;
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @Column({ unique: true, length: 100 })
-    username: string;
+  @Column({ unique: true, length: 100 })
+  username: string;
 
-    @Column({ unique: true, length: 255 })
-    email: string;
+  @Column({ unique: true, length: 255 })
+  email: string;
 
-    @Column({ length: 255 })
-    password: string;
+  @Column({ length: 255 })
+  password: string;
 
-    @OneToMany(() => Cv, (cv) => cv.user)
-    cvs: Cv[];
+  @Column({ default: 'user' })
+  role: string;
+
+  @OneToMany(() => Cv, (cv) => cv.user)
+  cvs: Cv[];
 }
